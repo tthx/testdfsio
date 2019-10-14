4bec81efc460e48f2f9d8bf500a87c759c9cf637
tthx <trinhthaihoa@gmail.com>
2019-10-14 16:04:07 +0200

    0.1

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-unset CXXFLAGS;
-export JAVA_HOME="/opt/jdk1.8.0_221";
-export JAVA_OPTS="-XX:+UseG1GC";
-export MAVEN_OPTS="${JAVA_OPTS} -Xms1g -Xmx1g";
-export PATH="${JAVA_HOME}/bin:${PATH}";
-mvn clean package;
